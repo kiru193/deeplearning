d838187289be1ae556e187d84d02f32ecfce1103
DESKTOP-FUK7EI4\zi-ku <zi-kuvurumu10922@hotmail.co.jp>
2023-05-16 01:01:49 +0900

    バイアスの導入 バイアスとは出力で1を得る度合いを調節するパラメータである

--- Python/Python.py
+++ Python/Python.py
@@ -1,12 +1,8 @@
-def AND(x1,x2):
-    w1,w2,theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
-        return 0
-    else:
-        return 1
+import numpy as np
+x = np.array([0,1])
+w = np.array([0.5,0.5])
+b = -0.7
 
-print(AND(0,0))
-print(AND(0,1))
-print(AND(1,0))
-print(AND(1,1))
+print(x*w)#各配列番号番号ごとかけ合わせる
+print(np.sum(x*w))#かけ合わせた総和
+print(np.sum(w*x)+b)#総和にバイアスを導入したもの
